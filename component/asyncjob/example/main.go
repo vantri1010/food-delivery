18165ae4f4cee7255ba7bb4ff49425992c9b9a48
Nguyen Van Tri <vantri1010@gmail.com>
2024-03-16 01:46:14 +0700

    execute job1 for example

--- component/asyncjob/example/main.go
+++ component/asyncjob/example/main.go
@@ -17,6 +17,24 @@ func main() {
 		return errors.New("something went wrong at job 1")
 	})
 
+	//if err := job1.Execute(context.Background()); err != nil {
+	//	log.Println("Job  is : ", job1.State(), err)
+	//
+	//	for {
+	//		if err := job1.Retry(context.Background()); err != nil {
+	//			log.Println("Retry with:", job1.RetryIndex(), "with state: ", job1.State(), err)
+	//		}
+	//
+	//		if job1.State() == asyncjob.StateRetryFailed {
+	//			break
+	//		}
+	//
+	//		if job1.State() == asyncjob.StateCompleted {
+	//			break
+	//		}
+	//	}
+	//}
+
 	job2 := asyncjob.NewJob(func(ctx context.Context) error {
 		time.Sleep(time.Second * 2)
 		log.Println("I am job 2")
